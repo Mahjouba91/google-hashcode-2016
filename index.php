733311bbe0f4fe1ac4b2fdc6bfb6a87e52ecb2c7
Florian TIAR <contact@tiar-florian.fr>
2016-02-14 18:35:06 +0100

    Update index

--- index.php
+++ index.php
@@ -6,41 +6,11 @@
  * Time: 20:23
  */
 
-class FileParser {
+require 'file-reader.php';
+require 'file-writer.php';
 
-	var $elements = array();
-	var $rows = array();
-	var $columns = array();
-	var $drones_nb = array();
-	var $deadline = array();
-	var $max_load = array();
+$readed_file = new File_Reader( "busy_day.in" );
+var_dump( $readed_file->first_line );
+echo $readed_file->first_line['rows'];
 
-	function __construct( $file_path ) {
-		$handle = @fopen( $file_path, "r" );
-		if ( $handle ) {
-			$i = 0;
-			$buffer = array();
-			while ( ($buffer[] = fgets($handle, 4096) ) !== false ) {
-				echo $buffer[$i] . '<br>';
-
-				$first_line = $buffer[0];
-	            $this->rows = $first_line[0];
-	            $this->columns = $first_line[1];
-	            $this->drones_nb = $first_line[2];
-	            $this->deadline = $first_line[3];
-	            $this->max_load = $first_line[4];
-
-				$i++;
-			}
-			if ( ! feof($handle) ) {
-				echo "Erreur: fgets() a échoué\n";
-			}
-			fclose($handle);
-		} else {
-			echo 'fichier non trouvé';
-		}
-	}
-
-}
-
-$parsed_file = new FileParser( "busy_day.in" );
+$writed_file = new File_Writer( "busy_day.out" );
