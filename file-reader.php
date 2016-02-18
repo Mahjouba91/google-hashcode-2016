158456cee073d008f81e46a95af3cd66c583638e
Florian TIAR <contact@tiar-florian.fr>
2016-02-18 16:02:54 +0100

    Comment params and code refactoring

--- file-reader.php
+++ file-reader.php
@@ -34,33 +34,33 @@ class File_Reader {
 			$i++;
 		}
 
-		$this->first_line['rows'] = (int) $buffer[0][0];
-		$this->first_line['columns'] = (int) $buffer[0][1];
-		$this->first_line['drones_nb'] = (int) $buffer[0][2];
-		$this->first_line['deadline'] = (int) $buffer[0][3];
-		$this->first_line['max_load'] = (int) $buffer[0][4];
+		$this->first_line['rows'] = (int) $buffer[0][0]; // rows count on the map
+		$this->first_line['columns'] = (int) $buffer[0][1]; // columns count on the map
+		$this->first_line['drones_nb'] = (int) $buffer[0][2]; // number of drones
+		$this->first_line['deadline'] = (int) $buffer[0][3]; // turns number of the simulation
+		$this->first_line['max_load'] = (int) $buffer[0][4]; // maximum supported drone weight
 
-		$this->p = (int) $buffer[1][0];
-		$this->weights = (int) $buffer[2];
+		$this->p = (int) $buffer[1][0]; // number of products
+		$this->weights = (int) $buffer[2]; // weight for each product type
 		$this->w = (int) $buffer[3][0]; // number of warehouses
 
 		$i = 0;
 		$last_index = 4;
 		for ( $j = 0; $j < $this->w; $j++ ) {
-			$this->warehouses[$j]['coords'] = $buffer[$i+$last_index];
-			$this->warehouses[$j]['p'] = $buffer[$i+$last_index+1];
+			$this->warehouses[$j]['coords'] = $buffer[ $last_index + $i ];
+			$this->warehouses[$j]['p'] = $buffer[ $last_index + $i +1 ];
 			$i += 2;
 		}
 
 		$last_index = ( $this->w * 2 ) + 4;
-		$this->c = (int) $buffer[$last_index][0];
+		$this->c = (int) $buffer[$last_index][0]; // number of orders
 		$last_index ++;
 
 		$i = 0;
 		for ( $j = 0; $j < $this->c; $j++ ) {
-			$this->orders[$j]['coords'] = $buffer[$i+$last_index];
-			$this->orders[$j]['num_of_items'] = $buffer[$i+$last_index+1][0];
-			$this->orders[$j]['p'] = $buffer[$i+$last_index+2];
+			$this->orders[$j]['coords'] = $buffer[ $last_index + $i ];
+			$this->orders[$j]['num_of_items'] = $buffer[ $last_index + $i +1 ][0];
+			$this->orders[$j]['p'] = $buffer[ $last_index + $i +2 ];
 			$i += 3;
 		}
 
