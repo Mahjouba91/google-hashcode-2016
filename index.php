4f61ea07fab388ee43b7b7ce271c2680dab2143b
Florian TIAR <contact@tiar-florian.fr>
2016-02-18 13:43:41 +0100

    Improve script

--- index.php
+++ index.php
@@ -1,14 +1,52 @@
 <?php
 
-require 'file-reader.php';
-require 'file-writer.php';
+/**
+ * Simple function to sort a multi dimensional array according to its values
+ *
+ * @param array $array
+ * @param string $key
+ * @param CONST int $order
+ * @param CONST int $sort_flag
+ *
+ * @return array
+ */
+function bea_array_sort( $array = array(), $key = '', $order = SORT_ASC, $sort_flag = SORT_REGULAR )
+{
+	$new_array = array();
+	$sortable_array = array();
 
-$readed_file = new File_Reader( "busy_day.in" );
-//var_dump( $readed_file->first_line );
-//echo $readed_file->first_line['drones_nb'];
+	if ( count($array) > 0 ) {
+		foreach ( $array as $k => $v ) {
+			if ( is_array($v) ) {
+				foreach ( $v as $k2 => $v2 ) {
+					if ( $k2 == $key ) {
+						$sortable_array[$k] = $v2;
+					}
+				}
+			} else {
+				$sortable_array[$k] = $v;
+			}
+		}
+
+		switch ( $order ) {
+			case SORT_ASC:
+				asort($sortable_array, $sort_flag);
+				break;
+			case SORT_DESC:
+				arsort($sortable_array, $sort_flag);
+				break;
+		}
+
+		foreach ( $sortable_array as $k => $v ) {
+			$new_array[$k] = $array[$k];
+		}
+	}
+
+	return $new_array;
+}
 
 function get_drone_distance( $coords_a, $coords_b ) {
-	$distance = sqrt ( pow( ($coords_a[0] - $coords_a[1]), 2 ) - pow( ($coords_b[0] - $coords_b[1]), 2 ) );
+	$distance = sqrt ( pow( ( (int) $coords_a[0] - (int) $coords_b[0]), 2 ) + pow( ( (int) $coords_a[1] - (int) $coords_b[1]), 2 ) );
 	$distance = ceil( $distance );
 	return $distance;
 }
@@ -25,15 +63,23 @@ function find_warehouse( $in_file, $product_id, $drone_position ) {
 			$i++;
 		}
 	}
-
-	var_dump( $found_warehouses );
-//	die();
-//	sort( $found_warehouses, 0 );
-
-	return false;
+	// Order by closest warehouses
+	$found_warehouses = bea_array_sort( $found_warehouses, 'dist', SORT_ASC );
+	return $found_warehouses;
 }
 
-find_warehouse( $readed_file, 6, array( 0, 0 ) );
+// Deliver the orders from the closest warehouse
+function sort_orders( $in_file, $warehouse_pos ) {
+	/** @var File_Reader $in_file */
 
-//$writed_file = new File_Writer( "busy_day.out" );
-
+	$sorted_orders = $in_file->orders;
+	foreach ( $in_file->orders as $order_id => $order ) {
+		$sorted_orders[$order_id]['id'] = $order_id;
+		$dist = get_drone_distance( $order['coords'], $warehouse_pos );
+		$sorted_orders[$order_id]['dist'] = $dist;
+	}
+	// Order by closest orders
+	$sorted_orders = bea_array_sort( $sorted_orders, 'dist', SORT_ASC );
+	var_dump($sorted_orders);
+	return $sorted_orders;
+}
